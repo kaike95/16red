345ae15a3cee057cd8a37b44d91606be963d6146
kaike95 <104430068+kaike95@users.noreply.github.com>
2023-07-10 13:06:44 -0300

    minor changes and cleanup

--- 16red.sh
+++ 16red.sh
@@ -32,7 +32,7 @@ ARRAYINDEX=0
 TEMPDIR=$(mktemp -d -t 16red-XXXXX) || error "'mktemp' command failed to create a temporary directory" 1
 
 quit() {
-	[[ $quit_counter ]] && {
+	[[ $quit_counter -eq 1 ]] && {
 		rm -r "${TEMPDIR}"
 		exit 130
 	} || echo -e "\n\033[0;33mPress one more time to end process. This will delete processing files.\033[0m"
@@ -40,7 +40,7 @@ quit() {
 }
 
 debug() {
-	[[ "${DEBUG}" -eq 1 ]] && echo -e "\033[0;33m$*\033[0m"
+	[[ $DEBUG -eq 1 ]] && echo -e "\033[0;33m$*\033[0m"
 }
 
 trap quit SIGINT
@@ -65,7 +65,7 @@ END
 
 # reduce() function:
 # Reduces input video to a filesize lower than the limit of 16MB
-# Global variables used: input_file, filequeue (array), info_checked, output_reduce_filename, output_dir, initial_filesize, bitrate_mode, FFMPEGLOGLEVEL, movflag, bitrate, TEMPDIR, filesize_reduction_total
+# Global variables used: input_file, filequeue (array), info_checked, output_reduce_filename, output_dir, initial_filesize, bitrate_mode, FFMPEGLOGLEVEL, movflag, bitrate, TEMPDIR, reduce_only
 # External programs used: ffmpeg
 # Requires: infocheck()
 
@@ -73,7 +73,7 @@ reduce() {
 
 	for input_file in "${filequeue[@]}"; do
 
-		[[ "${info_checked}" -eq 0 ]] && infocheck
+		[[ $info_checked -eq 0 ]] && infocheck
 
 		# if something wrong happens in infocheck() and doesn't exit with an error, go for the next file
 		[[ "$?" -eq 2 ]] && {
@@ -92,7 +92,7 @@ reduce() {
 		
 
 			#shellcheck disable=SC2086 #$FFMPEGLOGLEVEL doesn't work while "quoted"
-			if [[ "${bitrate_mode}" -eq 1 ]]; then
+			if [[ $bitrate_mode -eq 1 ]]; then
 
 				ffmpeg $FFMPEGLOGLEVEL -y -i "${input_file}" -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p -b:v "${bitrate}"k $movflag "${TEMPDIR}/${output_reduce_filename}"
 
@@ -109,7 +109,6 @@ reduce() {
 
 			reduce_checked=1
 			infocheck
-			echo "${filesize_reduction_total}"
 			mv "${TEMPDIR}/${output_reduce_filename}" "${output_dir}"
 			filequeue=( "${filequeue[@]:0:$ARRAYINDEX}" "${filequeue[@]:(($ARRAYINDEX+1))}" )
 			[[ $reduce_only -eq 0 ]] && format
@@ -127,10 +126,9 @@ reduce() {
 }
 
 
-
 # infocheck() function:
 # Gathers video data, such as filesize, length and ratio
-# Global variables used: info_checked, initial_filesize, input_file, file_aspectratio, length, DEBUG, bitrate_mode, bitrate, reduce_checked, final_filesize, filesize_reduction_total
+# Global variables used: info_checked, initial_filesize, input_file, file_aspectratio, length, DEBUG, bitrate_mode, bitrate, reduce_checked, final_filesize
 # External programs used: du, ffprobe
 # Requires: reduce()
 # Required by: reduce(), format(), smallcut()
@@ -143,7 +141,7 @@ infocheck() {
 
 		if [[ "${initial_filesize}" -eq 0 ]]; then
 
-			if [[ "${flag_all}" -eq 1 ]]; then
+			if [[ $flag_all -eq 1 ]]; then
 
 				echo "$input_file file size is 0, removing from queue"
 
@@ -151,7 +149,7 @@ infocheck() {
 				filequeue=( "${filequeue[@]:0:$ARRAYINDEX}" "${filequeue[@]:(($ARRAYINDEX+1))}" )
 				input_file="${filequeue[0]}"
 
-				[[ "${#filequeue[@]}" -eq 0 ]] && { error "no more files in filequeue, aborting..." 1 ; }
+				[[ "${#filequeue[@]}" -eq 0 ]] && error "no more files in filequeue, aborting..." 1
 
 				initial_filesize="$(cut -f1 <<< "$(du -k "$input_file")")"
 
@@ -166,10 +164,10 @@ infocheck() {
 		file_aspectratio=$(ffprobe "${input_file}" -show_entries stream=display_aspect_ratio -of csv=p=0:nk=1 -v 0)
 		#https://ffmpeg.org/ffprobe.html#compact_002c-csv
 
-		[[ -z "$file_aspectratio" ]] && { error "video could not get processed by FFprobe, cannot continue. Aborting..." 1; }
+		[[ -z "$file_aspectratio" ]] && error "video could not get processed by FFprobe, cannot continue. Aborting..." 1
 		length=$(printf '%.*f\n' 0 "$(ffprobe -i "$input_file" -v 16 -show_entries format=duration -of csv="p=0")") # output in seconds
 
-		[[ "${DEBUG}" -eq 1 ]] && cat <<END
+		[[ $DEBUG -eq 1 ]] && cat <<END
 
 DEBUG : infocheck() variables:
 
@@ -179,7 +177,7 @@ length : ${length}
 info_checked : ${info_checked}
 END
 
-		bitrate=$( printf '%.*f\n' 0 $(( 16000*4/length - 128))) #half the bitrate minus audio bitrate
+		bitrate=$(printf '%.*f\n' 0 $((16000*4/length - 128))) #half the bitrate minus audio bitrate
 		info_checked=1
 
 	fi
@@ -188,10 +186,9 @@ END
 	if [[ $reduce_checked -eq 1 ]]; then
 
 		final_filesize=$(cut -f1 <<< "$(du -k "${TEMPDIR}/${output_reduce_filename}")")
-		filesize_reduction_total=$(( initial_filesize - final_filesize ))
 		if [[ "${final_filesize}" -ge 16000 ]]; then
 			echo "Output filesize above 16MB, retrying with a lower bitrate"
-			bitrate=$((bitrate-256))
+			((bitrate-256))
 			reduce_checked=0
 			reduce
 			info_checked=0
@@ -242,7 +239,7 @@ format() {
 
 smallcut() {
 
-	[[ "${format_checked}" -eq 1 ]] && smallcut_input="${output_dir}/s-${input_file}" || smallcut_input="${input_file}"
+	[[ $format_checked -eq 1 ]] && smallcut_input="${output_dir}/s-${input_file}" || smallcut_input="${input_file}"
 	# If format() has run successfully, change $smallcut_input to it's output, if not set to input_file, as it's already formatted
 
 	local _total_cuts=$(( length/15 ))
@@ -258,7 +255,7 @@ smallcut() {
 
 	for ((_cutloop=0; _cutloop <= _total_cuts; _cutloop++)); do
 
-		echo "Cut $((_cutloop+1))"
+		echo "Cut $((_cutloop++))"
 		local _output_smallcut_filename="${TEMPDIR}/s-${_cutloop}-${input_file}"
 		#shellcheck disable=SC2086
 		ffmpeg $FFMPEGLOGLEVEL -ss "${_cut_count_final}" -i "${smallcut_input}" -t 14.900 $_copyflag $movflag -b:v "${bitrate}"k "${_output_smallcut_filename}"
@@ -267,8 +264,8 @@ smallcut() {
 		_copyflag=""
 
 		# replaces bc with scientific notation using printf # shell-tips.com/bash/math-arithmetic-calculation
-		_cut_count=$(( 149 + _cut_count ))
-		_cut_count_final=$( printf %.1f "$(( _cut_count ))e-1" )
+		_cut_count=$((149+_cut_count))
+		_cut_count_final=$(printf %.1f "$((_cut_count))e-1")
 
 		_cut_files+=("${_output_smallcut_filename}")
 
@@ -278,6 +275,7 @@ smallcut() {
 	mv "${_cut_files[@]}" "${output_dir}"
 }
 
+[[ $# -eq 1 ]] && usage # no args
 
 while getopts ":dhablmfi" options; do
              # ^ silent mode getopts
@@ -295,9 +293,11 @@ while getopts ":dhablmfi" options; do
 	esac
 done
 
+[[ $# -ge 2 ]] && shift $((OPTIND--))
+
 #Print variables if $DEBUG is on
 
-[[ "${DEBUG}" -eq 1 ]] && cat <<END
+[[ $DEBUG -eq 1 ]] && cat <<END
 16red starting...
 
 Temporary Directory location: "${TEMPDIR}"
@@ -308,17 +308,13 @@ bitrate_mode = $bitrate_mode
 
 END
 
-[[ $# -ge 2 ]] && shift $((OPTIND-1))
-
-[[ "${flag_all}" -eq 0 ]] && {
+[[ $flag_all -eq 0 ]] && {
 
 		for argument_validation in "$@"; do
 
 			if [[ "$(file -b --mime-type "${argument_validation}")" =~ "video" && -e "${argument_validation}" ]]; then
 				valid_argument="${argument_validation}"
-				[[ -d "${argument_validation}" ]] && {
-					error "${argument_validation} is a directory, aborting..." 1
-				}
+				[[ -d "${argument_validation}" ]] && error "${argument_validation} is a directory, aborting..." 1
 				break
 				# if nothing matches, this passes "" (nothing), seeking alternatives to this
 			fi
@@ -339,23 +335,21 @@ END
 		[[ -f "${valid_argument}" ]] && filequeue+=("${valid_argument}")
 		debug "getopts : '${valid_argument}' added to filequeue, it now has ${#filequeue[@]} item(s)"
 
-		reduce
-
 }
 
-[[ "${flag_all}" -eq 1 ]] && {
+[[ $flag_all -eq 1 ]] && {
 
 	for file in *; do
 
 		[[ "$(file -b --mime-type "${file}")" =~ "video" && ! -d "${file}" ]] && filequeue+=("${file}")
+		debug "getopts : '${valid_argument}' added to filequeue, it now has ${#filequeue[@]} item(s)"
 
 	done
 
 	[[ "${#filequeue[@]}" -eq 0 ]] && error "No valid files found" 1
 
 	debug "Files found: " "${filequeue[@]}"
-	reduce
 
 }
 
-[ -z "$*" ] && usage #SC2198
+reduce
