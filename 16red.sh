860ea1c8ed912101f007f0d81cbc217bcb62dd03
kaike95 <104430068+kaike95@users.noreply.github.com>
2023-12-31 14:01:42 -0300

    small fixes and changes to tests

--- 16red.sh
+++ 16red.sh
@@ -3,16 +3,14 @@
 #O código e os comentários estarão em INGLÊS!
 
 #Start date: December 8 2022
-#Last Update: July 10 2023
+#Last Update: December 31 2023
 #External help: aakova, TomJo2000
 #Purpose: Reduce video filesize to <16MB, optionally format video to 9:16 using black bars and cut formatted video into ≃14.9 second segments.
-
-#TODO: interactive mode (prompt for changes)
 
 #Dependency check:
 
 error() {
-	>&2 echo -e "\033[0;31mError: ${1:-on line $LINENO - non specified}\033[0m"
+	>&2 echo -e "\033[0;31mError: ${1:-on line $LINENO - not specified}\033[0m"
 	exit "${2}"
 }
 
@@ -31,8 +29,8 @@ flag_all=0
 ARRAYINDEX=0
 TEMPDIR=$(mktemp -d -t 16red-XXXXX) || error "'mktemp' command failed to create a temporary directory" 1
 
-quit() {
-	[[ $quit_counter -eq 1 ]] && {
+quit() { # this does not work as intended, ffmpeg gets killed first by SIGINT and then this function runs
+	(( quit_counter )) && {
 		rm -r "${TEMPDIR}"
 		exit 130
 	} || echo -e "\n\033[0;33mPress one more time to end process. This will delete processing files.\033[0m"
@@ -40,10 +38,9 @@ quit() {
 }
 
 debug() {
-	[[ $DEBUG -eq 1 ]] && echo -e "\033[0;33m$*\033[0m"
+	(( DEBUG )) && echo -e "\033[0;33m$*\033[0m"
+	return 0
 }
-
-trap quit SIGINT
 
 usage() {
 	cat <<END
@@ -54,8 +51,6 @@ usage() {
 -l : Toggles FFmpeg log level to normal. Disabled by default
 -m : Enables -movflag faststart, might take longer to start but helps with playback issues
 -f : Only reduces the video size, doesn't format into 9:16
-not implemented:
--i : Toggles interactive mode, prompting for certain options
 Usage example: ${0} (-b) (-d) file.mp4
 
 END
@@ -73,10 +68,10 @@ reduce() {
 
 	for input_file in "${filequeue[@]}"; do
 
-		[[ $info_checked -eq 0 ]] && infocheck
+		(( info_checked )) || infocheck
 
 		# if something wrong happens in infocheck() and doesn't exit with an error, go for the next file
-		[[ "$?" -eq 2 ]] && {
+		(( "$?" == 2 )) && {
 
 		info_checked=0
 		input_file="${filequeue[0]}"
@@ -86,13 +81,13 @@ reduce() {
 
 		output_reduce_filename="${input_file}"
 		output_dir="${PWD}/vid-${input_file}"
-		mkdir -p "${output_dir}"
+		mkdir -p "${output_dir}" || error "Could not create a directory in $PWD" 1
 
-		if [[ "${initial_filesize}" -ge 16000 ]]; then
+		if (( initial_filesize >= 16000 )); then
 		
 
 			#shellcheck disable=SC2086 #$FFMPEGLOGLEVEL doesn't work while "quoted"
-			if [[ $bitrate_mode -eq 1 ]]; then
+			if (( bitrate_mode )); then
 
 				ffmpeg $FFMPEGLOGLEVEL -y -i "${input_file}" -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p -b:v "${bitrate}"k $movflag "${TEMPDIR}/${output_reduce_filename}"
 
@@ -100,58 +95,59 @@ reduce() {
 
 				debug "\nreduce() : found '${input_file}'\nbitrate='${bitrate}'"
 
-				echo "${input_file} : Pass 1"
+				debug "${input_file} : Pass 1"
 				ffmpeg $FFMPEGLOGLEVEL -y -i "${input_file}" -c:v libx264 -pix_fmt yuv420p -b:v "${bitrate}"k -pass 1 -vsync cfr -f null /dev/null &&	\
-				echo "${input_file} : Pass 2" && \
+				debug "${input_file} : Pass 2" && \
 				ffmpeg $FFMPEGLOGLEVEL -i "${input_file}" -c:v libx264 -pix_fmt yuv420p -b:v "${bitrate}"k -pass 2 -c:a copy $movflag "${TEMPDIR}/${output_reduce_filename}"
 
 			fi
 
 			reduce_checked=1
 			infocheck
-			mv "${TEMPDIR}/${output_reduce_filename}" "${output_dir}"
-			filequeue=( "${filequeue[@]:0:$ARRAYINDEX}" "${filequeue[@]:(($ARRAYINDEX+1))}" )
-			[[ $reduce_only -eq 0 ]] && format
+			mv "${TEMPDIR}/${output_reduce_filename}" "${output_dir}" 2> /dev/null  || error "mv returned non-0 exit code" 1
+			echo "Finished reducing '${input_file}'"
 
 		else
 
 			echo "Target video under 16MB, skipping"; #TODO add $INTERACTIVEMODE
-			filequeue=( "${filequeue[@]:0:$ARRAYINDEX}" "${filequeue[@]:(($ARRAYINDEX+1))}" )
-			[[ $reduce_only -eq 0 ]] && format
 
 		fi
 
-	done
+			filequeue=( "${filequeue[@]:0:$ARRAYINDEX}" "${filequeue[@]:(($ARRAYINDEX+1))}" )
+			(( reduce_only )) || format
 
+	done
 }
 
 
 # infocheck() function:
 # Gathers video data, such as filesize, length and ratio
-# Global variables used: info_checked, initial_filesize, input_file, file_aspectratio, length, DEBUG, bitrate_mode, bitrate, reduce_checked, final_filesize
+# Global variables used: info_checked, initial_filesize, dusize, input_file, file_aspectratio, length, lengthseconds, DEBUG, bitrate_mode, bitrate, reduce_checked, final_filesize
 # External programs used: du, ffprobe
 # Requires: reduce()
 # Required by: reduce(), format(), smallcut()
 
 
 infocheck() {
-	if [[ $info_checked -eq 0 ]]; then
+	if (( ! info_checked )); then
 
-		initial_filesize="$(cut -f1 <<< "$(du -k "$input_file")")"
+		dusize="$(du -k "${input_file}" 2> /dev/null)" || error "'du' returned a non-0 exit code" 1
+		initial_filesize="$(cut -f1 <<< "$dusize")"
 
-		if [[ "${initial_filesize}" -eq 0 ]]; then
+		if (( ! initial_filesize )); then
 
-			if [[ $flag_all -eq 1 ]]; then
+			if (( flag_all )); then
 
-				echo "$input_file file size is 0, removing from queue"
+				debug "$input_file file size is 0, removing from queue"
 
 				# unsets the current file in queue, go for the next
 				filequeue=( "${filequeue[@]:0:$ARRAYINDEX}" "${filequeue[@]:(($ARRAYINDEX+1))}" )
+				(( "${#filequeue[@]}" )) || error "no more files in filequeue, aborting..." 1
+
 				input_file="${filequeue[0]}"
 
-				[[ "${#filequeue[@]}" -eq 0 ]] && error "no more files in filequeue, aborting..." 1
-
-				initial_filesize="$(cut -f1 <<< "$(du -k "$input_file")")"
+				dusize="$(du -k "${input_file}" 2> /dev/null)" || error "'du' returned a non-0 exit code" 1
+				initial_filesize="$(cut -f1 <<< "$dusize")"
 
 			else
 
@@ -161,13 +157,16 @@ infocheck() {
 
 		fi
 
-		file_aspectratio=$(ffprobe "${input_file}" -show_entries stream=display_aspect_ratio -of csv=p=0:nk=1 -v 0)
+		file_aspectratio=$(ffprobe "${input_file}" -show_entries stream=display_aspect_ratio -of csv=p=0:nk=1 -v 0 2> /dev/null ) \
+			|| error "ffprobe returned a non-0 exit code" 1
 		#https://ffmpeg.org/ffprobe.html#compact_002c-csv
 
 		[[ -z "$file_aspectratio" ]] && error "video could not get processed by FFprobe, cannot continue. Aborting..." 1
-		length=$(printf '%.*f\n' 0 "$(ffprobe -i "$input_file" -v 16 -show_entries format=duration -of csv="p=0")") # output in seconds
 
-		[[ $DEBUG -eq 1 ]] && cat <<END
+		lengthseconds="$(ffprobe -i "${input_file}" -v 16 -show_entries format=duration -of csv="p=0")" \
+			|| error "ffprobe returned a non-0 exit code" 1
+		length=$(printf '%.*f\n' 0 "$lengthseconds")
+		(( DEBUG )) && cat <<END
 
 DEBUG : infocheck() variables:
 
@@ -183,10 +182,11 @@ END
 	fi
 
 	# this only runs when reduce() has run at least once, due to $reduce_checked, then resets for future use of the function
-	if [[ $reduce_checked -eq 1 ]]; then
-
-		final_filesize=$(cut -f1 <<< "$(du -k "${TEMPDIR}/${output_reduce_filename}")")
-		if [[ "${final_filesize}" -ge 16000 ]]; then
+	if (( reduce_checked )); then
+		
+		dusize="$(du -k "${TEMPDIR}/${output_reduce_filename}" 2> /dev/null)"
+		final_filesize=$(cut -f1 <<< "$dusize")
+		if (( final_filesize >= 16000 )); then
 			echo "Output filesize above 16MB, retrying with a lower bitrate"
 			((bitrate-256))
 			reduce_checked=0
@@ -215,13 +215,14 @@ format() {
 
 		#shellcheck disable=SC2086
 		ffmpeg $FFMPEGLOGLEVEL -y -i "${input_file}" -vf 'scale=1080:1920:force_original_aspect_ratio=decrease,pad=1080:1920:-1:-1:color=black' $movflag -b:v "${bitrate}"k "${TEMPDIR}/s-${input_file}"
-		mv "${TEMPDIR}/s-${input_file}" "${output_dir}";
+		mv "${TEMPDIR}/s-${input_file}" "${output_dir}" 2> /dev/null || error "mv returned a non-0 exit code" 1;
 		format_checked=1
+		echo "Finished formatting '${input_file}' to 9:16"
 		smallcut
 
 	else
 
-		echo -e "format() : ${input_file} has the correct aspect-ratio of 9:16, skipping...\n"
+		debug "format() : ${input_file} has the correct aspect-ratio of 9:16, skipping..."
 		smallcut
 
 	fi
@@ -239,13 +240,13 @@ format() {
 
 smallcut() {
 
-	[[ $format_checked -eq 1 ]] && smallcut_input="${output_dir}/s-${input_file}" || smallcut_input="${input_file}"
+	(( format_checked )) && smallcut_input="${output_dir}/s-${input_file}" || smallcut_input="${input_file}"
 	# If format() has run successfully, change $smallcut_input to it's output, if not set to input_file, as it's already formatted
 
 	local _total_cuts=$(( length/15 ))
 
-	[[ "${_total_cuts}" -lt 1 ]] && {
-		echo "Passed video is smaller than 15 seconds, skipping..."
+	(( _total_cuts < 1 )) && {
+		echo "Passed video is smaller than 15 seconds, skipping cuts..."
 		return 0
 	}
 
@@ -255,7 +256,7 @@ smallcut() {
 
 	for ((_cutloop=0; _cutloop <= _total_cuts; _cutloop++)); do
 
-		echo "Cut $((_cutloop++))"
+		echo "Cut $((_cutloop+1))"
 		local _output_smallcut_filename="${TEMPDIR}/s-${_cutloop}-${input_file}"
 		#shellcheck disable=SC2086
 		ffmpeg $FFMPEGLOGLEVEL -ss "${_cut_count_final}" -i "${smallcut_input}" -t 14.900 $_copyflag $movflag -b:v "${bitrate}"k "${_output_smallcut_filename}"
@@ -272,10 +273,10 @@ smallcut() {
 	done
 
 	mkdir -p "${output_dir}"
-	mv "${_cut_files[@]}" "${output_dir}"
+	mv "${_cut_files[@]}" "${output_dir}" || error "mv returned non-0 exit code" 1
 }
 
-[[ $# -eq 1 ]] && usage # no args
+(( $# )) || usage # no args
 
 while getopts ":dhablmfi" options; do
              # ^ silent mode getopts
@@ -293,11 +294,9 @@ while getopts ":dhablmfi" options; do
 	esac
 done
 
-[[ $# -ge 2 ]] && shift $((OPTIND--))
-
 #Print variables if $DEBUG is on
 
-[[ $DEBUG -eq 1 ]] && cat <<END
+(( DEBUG )) && cat <<END
 16red starting...
 
 Temporary Directory location: "${TEMPDIR}"
@@ -308,36 +307,9 @@ bitrate_mode = $bitrate_mode
 
 END
 
-[[ $flag_all -eq 0 ]] && {
+(( $# > 2 )) && shift "$((OPTIND-1))"
 
-		for argument_validation in "$@"; do
-
-			if [[ "$(file -b --mime-type "${argument_validation}")" =~ "video" && -e "${argument_validation}" ]]; then
-				valid_argument="${argument_validation}"
-				[[ -d "${argument_validation}" ]] && error "${argument_validation} is a directory, aborting..." 1
-				break
-				# if nothing matches, this passes "" (nothing), seeking alternatives to this
-			fi
-
-		done
-
-		debug "Argument passed: ${argument_validation}\nArgument validated: ${valid_argument}"
-
-		if [[ ! "$(file -b --mime-type "${valid_argument}")" =~ "video" || -z "${valid_argument}" ]]; then
-
-			[[ "${#filequeue[@]}" -eq 0 ]] &&	error "No files in filequeue, did you forget to specify a file?" 1
-
-			#if no argument matches, it causes the last argument to be passed to $argument, this double checks it
-			error "${valid_argument} : Passed video does not exist, is not supported or it is an option" 1
-
-		fi
-
-		[[ -f "${valid_argument}" ]] && filequeue+=("${valid_argument}")
-		debug "getopts : '${valid_argument}' added to filequeue, it now has ${#filequeue[@]} item(s)"
-
-}
-
-[[ $flag_all -eq 1 ]] && {
+if (( flag_all )); then
 
 	for file in *; do
 
@@ -346,10 +318,37 @@ END
 
 	done
 
-	[[ "${#filequeue[@]}" -eq 0 ]] && error "No valid files found" 1
+	(( "${#filequeue[@]}" )) || error "No valid files found" 1
 
 	debug "Files found: " "${filequeue[@]}"
 
-}
+else
+
+	for argument_validation in "$@"; do
+
+		if [[ "$(file -b --mime-type "${argument_validation}")" =~ "video" && -e "${argument_validation}" ]]; then
+			valid_argument="${argument_validation}"
+			[[ -d "${argument_validation}" ]] && error "${argument_validation} is a directory, aborting..." 1
+			break
+			# if nothing matches, this passes "" (nothing), seeking alternatives to this
+		fi
+
+	done
+
+	debug "Argument passed: ${argument_validation}\nArgument validated: ${valid_argument}"
+
+	if [[ ! "$(file -b --mime-type "${valid_argument}")" =~ "video" || -z "${valid_argument}" ]]; then
+
+		(( "${#filequeue[@]}" )) ||	error "No files in filequeue, did you forget to specify a file?" 1
+
+		#if no argument matches, it causes the last argument to be passed to $argument, this double checks it
+		error "${valid_argument} : Passed video does not exist, is not supported or it is an option" 1
+
+	fi
+
+	[[ -f "${valid_argument}" ]] && filequeue+=("${valid_argument}")
+	debug "getopts : '${valid_argument}' added to filequeue, it now has ${#filequeue[@]} item(s)"
+
+fi
 
 reduce
